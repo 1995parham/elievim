53826a9d13690eefa1b7edeb90c51e3d2f062064
Parham Alvani <parham.alvani@gmail.com>
2022-08-01 08:25:42 +0430

    Initial commit

---
+++ lua/keymap/init.lua
@@ -0,0 +1,29 @@
+-- author: glepnr https://github.com/glepnir
+-- date: 2022-07-02
+-- License: MIT
+-- recommend plugins key defines in this file
+
+require('keymap.config')
+local key = require('core.keymap')
+local nmap = key.nmap
+local silent, noremap = key.silent, key.noremap
+local opts = key.new_opts
+local cmd = key.cmd
+
+-- usage of plugins
+nmap({
+  -- packer
+  { '<Leader>pu', cmd('PackerUpdate'), opts(noremap, silent) },
+  { '<Leader>pi', cmd('PackerInstall'), opts(noremap, silent) },
+  { '<Leader>pc', cmd('PackerCompile'), opts(noremap, silent) },
+  -- dashboard
+  { '<Leader>n', cmd('DashboardNewFile'), opts(noremap, silent) },
+  { '<Leader>ss', cmd('SessionSave'), opts(noremap, silent) },
+  { '<Leader>sl', cmd('SessionLoad'), opts(noremap, silent) },
+  -- nvimtree
+  { '<Leader>e', cmd('NvimTreeToggle'), opts(noremap, silent) },
+  -- Telescope
+  { '<Leader>b', cmd('Telescope buffers'), opts(noremap, silent) },
+  { '<Leader>fa', cmd('Telescope live_grep'), opts(noremap, silent) },
+  { '<Leader>ff', cmd('Telescope find_files'), opts(noremap, silent) },
+})
---
+++ lua/modules/completion/config.lua
@@ -0,0 +1,44 @@
+-- author: glepnr https://github.com/glepnir
+-- date: 2022-07-02
+-- License: MIT
+
+local config = {}
+
+-- config server in this function
+function config.nvim_lsp() end
+
+function config.nvim_cmp()
+  local cmp = require('cmp')
+
+  cmp.setup({
+    preselect = cmp.PreselectMode.Item,
+    window = {
+      completion = cmp.config.window.bordered(),
+      documentation = cmp.config.window.bordered(),
+    },
+  })
+end
+
+function config.lua_snip()
+  local ls = require('luasnip')
+  local types = require('luasnip.util.types')
+  ls.config.set_config({
+    history = true,
+    enable_autosnippets = true,
+    updateevents = 'TextChanged,TextChangedI',
+    ext_opts = {
+      [types.choiceNode] = {
+        active = {
+          virt_text = { { '<- choiceNode', 'Comment' } },
+        },
+      },
+    },
+  })
+  require('luasnip.loaders.from_lua').lazy_load({ paths = vim.fn.stdpath('config') .. '/snippets' })
+  require('luasnip.loaders.from_vscode').lazy_load()
+  require('luasnip.loaders.from_vscode').lazy_load({
+    paths = { './snippets/' },
+  })
+end
+
+return config
