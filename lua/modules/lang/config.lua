8d6915f74fabc7d3295fe2f06e3c8395122540e3
Parham Alvani <parham.alvani@gmail.com>
2022-08-04 07:42:42 +0430

    feat: switch completely to treesitter

--- lua/modules/lang/config.lua
+++ lua/modules/lang/config.lua
@@ -4,8 +4,8 @@ function config.nvim_treesitter()
   vim.api.nvim_command('set foldmethod=expr')
   vim.api.nvim_command('set foldexpr=nvim_treesitter#foldexpr()')
   require('nvim-treesitter.configs').setup({
-    ensure_installed = { "lua", "go", "rust", "c", "bash", "json", "yaml", "python", "javascript", "cpp" },
-    ignore_install = {},
+    ensure_installed = "all",
+    ignore_install = { "phpdoc" },
     highlight = {
       enable = true,
     },
