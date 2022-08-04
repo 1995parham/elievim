20def279aaf9327a850acdac61efc19a42b95044
Parham Alvani <parham.alvani@gmail.com>
2022-08-05 02:06:36 +0430

    feat: resolve null-ls issue

--- lua/modules/completion/config.lua
+++ lua/modules/completion/config.lua
@@ -87,31 +87,8 @@ end
 
 function config.mason.lspconfig()
   local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+  local lsp_on_attach = require('modules.completion.on_attach')
   capabilities.textDocument.completion.completionItem.snippetSupport = true
-
-  local on_attach = function(_, bufnr)
-    -- Enable completion triggered by <c-x><c-o>
-    vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
-
-    -- Mappings.
-    -- See `:help vim.lsp.*` for documentation on any of the below functions
-    local bufopts = { noremap = true, silent = true, buffer = bufnr }
-    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
-    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
-    vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
-    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
-    vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
-    vim.keymap.set('n', '<Leader>wa', vim.lsp.buf.add_workspace_folder, bufopts)
-    vim.keymap.set('n', '<Leader>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
-    vim.keymap.set('n', '<Leader>wl', function()
-      print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-    end, bufopts)
-    vim.keymap.set('n', '<Leader>D', vim.lsp.buf.type_definition, bufopts)
-    vim.keymap.set('n', '<Leader>rn', vim.lsp.buf.rename, bufopts)
-    vim.keymap.set('n', '<Leader>ca', vim.lsp.buf.code_action, bufopts)
-    vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
-    vim.keymap.set('n', '<Leader>f', vim.lsp.buf.formatting, bufopts)
-  end
 
   require('mason-lspconfig').setup()
   require('mason-lspconfig').setup_handlers({
@@ -120,7 +97,7 @@ function config.mason.lspconfig()
     -- a dedicated handler.
     function(server_name) -- default handler (optional)
       require('lspconfig')[server_name].setup({
-        on_attach = on_attach,
+        on_attach = lsp_on_attach,
         capabilities = capabilities,
       })
     end,
@@ -129,29 +106,16 @@ end
 
 function config.null_ls()
   local null_ls = require('null-ls')
-  local augroup = vim.api.nvim_create_augroup('LspFormatting', {})
+  local lsp_on_attach = require('modules.completion.on_attach')
 
   null_ls.setup({
     -- you can reuse a shared lspconfig on_attach callback here
-    on_attach = function(client, bufnr)
-      if client.supports_method('textDocument/formatting') then
-        vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-        vim.api.nvim_create_autocmd('BufWritePre', {
-          group = augroup,
-          buffer = bufnr,
-          callback = function()
-            -- on 0.8, you should use
-            -- vim.lsp.buf.format({ bufnr = bufnr }) instead
-            vim.lsp.buf.formatting_sync()
-          end,
-        })
-      end
-    end,
+    on_attach = lsp_on_attach,
     sources = {
       null_ls.builtins.formatting.stylua,
       null_ls.builtins.formatting.sql_formatter,
       null_ls.builtins.formatting.jq,
-      -- null_ls.builtins.formatting.gofumpt,
+      null_ls.builtins.formatting.gofumpt,
 
       -- null_ls.builtins.diagnostics.golangci_lint,
       null_ls.builtins.diagnostics.jsonlint,
---
+++ lua/modules/completion/on_attach.lua
@@ -0,0 +1,46 @@
+local lsp_formatting = function(bufnr)
+  vim.lsp.buf.format({
+    filter = function(client)
+      -- apply whatever logic you want (in this example, we'll only use null-ls)
+      return client.name == 'null-ls'
+    end,
+    bufnr = bufnr,
+  })
+end
+
+return function(client, bufnr)
+  local augroup = vim.api.nvim_create_augroup('LspFormatting', {})
+
+  -- Enable completion triggered by <c-x><c-o>
+  vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+
+  -- Mappings.
+  -- See `:help vim.lsp.*` for documentation on any of the below functions
+  local bufopts = { noremap = true, silent = true, buffer = bufnr }
+  vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
+  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
+  vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
+  vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
+  vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
+  vim.keymap.set('n', '<Leader>wa', vim.lsp.buf.add_workspace_folder, bufopts)
+  vim.keymap.set('n', '<Leader>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
+  vim.keymap.set('n', '<Leader>wl', function()
+    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+  end, bufopts)
+  vim.keymap.set('n', '<Leader>D', vim.lsp.buf.type_definition, bufopts)
+  vim.keymap.set('n', '<Leader>rn', vim.lsp.buf.rename, bufopts)
+  vim.keymap.set('n', '<Leader>ca', vim.lsp.buf.code_action, bufopts)
+  vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
+  vim.keymap.set('n', '<Leader>f', vim.lsp.buf.formatting, bufopts)
+
+  if client.supports_method('textDocument/formatting') then
+    vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+    vim.api.nvim_create_autocmd('BufWritePre', {
+      group = augroup,
+      buffer = bufnr,
+      callback = function()
+        lsp_formatting(bufnr)
+      end,
+    })
+  end
+end
