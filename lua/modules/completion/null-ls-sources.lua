21aea1200f8d5f1e578dd2f5e230ebfb70e754a8
Parham Alvani <parham.alvani@gmail.com>
2022-08-06 01:16:33 +0430

    feat: even more shellchecker

--- lua/modules/completion/null-ls-sources.lua
+++ lua/modules/completion/null-ls-sources.lua
@@ -1,6 +1,8 @@
 local null_ls = require('null-ls')
 
 return {
+  null_ls.builtins.code_actions.shellcheck,
+
   null_ls.builtins.formatting.stylua,
   null_ls.builtins.formatting.sql_formatter,
   null_ls.builtins.formatting.jq,
@@ -19,4 +21,5 @@ return {
   null_ls.builtins.diagnostics.djlint,
   null_ls.builtins.diagnostics.actionlint,
   null_ls.builtins.diagnostics.markdownlint,
+  null_ls.builtins.diagnostics.shellcheck,
 }
