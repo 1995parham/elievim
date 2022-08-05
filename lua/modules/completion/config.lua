d58d04a56aebe7c2fde722554851d9a1237fece6
Parham Alvani <parham.alvani@gmail.com>
2022-08-05 12:04:41 +0430

    feat: add action lint

--- lua/modules/completion/config.lua
+++ lua/modules/completion/config.lua
@@ -131,6 +131,7 @@ function config.null_ls()
       -- null_ls.builtins.diagnostics.pyproject_flake8,
       null_ls.builtins.diagnostics.flake8,
       null_ls.builtins.formatting.black,
+      null_ls.builtins.diagnostics.actionlint,
     },
   })
 end
--- lua/modules/lang/config.lua
+++ lua/modules/lang/config.lua
@@ -1,8 +1,8 @@
 local config = {}
 
 function config.nvim_treesitter()
-  vim.api.nvim_command('set foldmethod=expr')
-  vim.api.nvim_command('set foldexpr=nvim_treesitter#foldexpr()')
+  -- vim.api.nvim_command('set foldmethod=expr')
+  -- vim.api.nvim_command('set foldexpr=nvim_treesitter#foldexpr()')
   require('nvim-treesitter.configs').setup({
     ensure_installed = 'all',
     ignore_install = { 'phpdoc' },
