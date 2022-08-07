1e09b650cb98335629b0a37ba37eec2fb8340309
Parham Alvani <parham.alvani@gmail.com>
2022-08-07 10:46:23 +0430

    feat: change default black configuration

--- lua/modules/completion/null-ls-sources.lua
+++ lua/modules/completion/null-ls-sources.lua
@@ -8,7 +8,9 @@ return {
   null_ls.builtins.formatting.jq,
   null_ls.builtins.formatting.gofumpt,
   null_ls.builtins.formatting.markdownlint,
-  null_ls.builtins.formatting.black,
+  null_ls.builtins.formatting.black.with({
+    extra_args = { '-l', '79' },
+  }),
   null_ls.builtins.formatting.djlint,
   null_ls.builtins.formatting.taplo,
   null_ls.builtins.formatting.shfmt,
