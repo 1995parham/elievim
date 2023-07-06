57f6a18277fd0a48f93b7508de21d9d113e94caf
Parham Alvani <parham.alvani@gmail.com>
2023-07-06 23:22:06 +0330

    feat: add new null-ls formatter

--- lua/modules/completion/null-ls-sources.lua
+++ lua/modules/completion/null-ls-sources.lua
@@ -41,4 +41,5 @@ return {
   null_ls.builtins.diagnostics.hadolint,
   -- null_ls.builtins.diagnostics.markdownlint,
   null_ls.builtins.diagnostics.shellcheck,
+  null_ls.builtins.formatting.just,
 }
