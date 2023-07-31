c0ecd2e20630b7e0d9180f07e47e83d3df1d3f39
Parham Alvani <parham.alvani@gmail.com>
2023-07-31 08:00:27 +0330

    feat: add new keymap for my terminals

--- lua/keymap/init.lua
+++ lua/keymap/init.lua
@@ -10,27 +10,27 @@ local silent, noremap = key.silent, key.noremap
 local opts = key.new_opts
 local cmd = key.cmd
 
--- usage of plugins
+-- normal mode key mapping
 nmap({
   -- packer
-  { '<Leader>pu',  cmd('PackerUpdate'),           opts(noremap, silent) },
-  { '<Leader>pi',  cmd('PackerInstall'),          opts(noremap, silent) },
-  { '<Leader>pc',  cmd('PackerCompile'),          opts(noremap, silent) },
+  { '<Leader>pu', cmd('PackerUpdate'), opts(noremap, silent) },
+  { '<Leader>pi', cmd('PackerInstall'), opts(noremap, silent) },
+  { '<Leader>pc', cmd('PackerCompile'), opts(noremap, silent) },
   -- dashboard
-  { '<Leader>n',   cmd('DashboardNewFile'),       opts(noremap, silent) },
-  { '<Leader>ss',  cmd('SessionSave'),            opts(noremap, silent) },
-  { '<Leader>sl',  cmd('SessionLoad'),            opts(noremap, silent) },
+  { '<Leader>n', cmd('DashboardNewFile'), opts(noremap, silent) },
+  { '<Leader>ss', cmd('SessionSave'), opts(noremap, silent) },
+  { '<Leader>sl', cmd('SessionLoad'), opts(noremap, silent) },
   -- nvimtree
-  { '<Leader>pff', cmd('NvimTreeToggle'),         opts(noremap, silent) },
+  { '<Leader>pff', cmd('NvimTreeToggle'), opts(noremap, silent) },
   -- Telescope
-  { '<Leader>bb',  cmd('Telescope buffers'),      opts(noremap, silent, 'buffer browser') },
-  { '<Leader>man', cmd('Telescope man_pages'),    opts(noremap, silent) },
-  { '<Leader>fa',  cmd('Telescope live_grep'),    opts(noremap, silent) },
-  { '<Leader>ff',  cmd('Telescope file_browser'), opts(noremap, silent, 'file browser') },
-  { '<leader>sf',  cmd('Telescope find_files'),   opts(noremap, silent, '[s]earch [f]iles') },
+  { '<Leader>bb', cmd('Telescope buffers'), opts(noremap, silent, 'buffer browser') },
+  { '<Leader>man', cmd('Telescope man_pages'), opts(noremap, silent) },
+  { '<Leader>fa', cmd('Telescope live_grep'), opts(noremap, silent) },
+  { '<Leader>ff', cmd('Telescope file_browser'), opts(noremap, silent, 'file browser') },
+  { '<leader>sf', cmd('Telescope find_files'), opts(noremap, silent, '[s]earch [f]iles') },
   -- buffers
-  { '<Leader>bn',  cmd('bnext'),                  opts(noremap, silent, 'next buffer') },
-  { '<Leader>bn',  cmd('bprevious'),              opts(noremap, silent, 'previous buffer') },
+  { '<Leader>bn', cmd('bnext'), opts(noremap, silent, 'next buffer') },
+  { '<Leader>bn', cmd('bprevious'), opts(noremap, silent, 'previous buffer') },
   -- Panda
   {
     '<leader>dd',
@@ -39,21 +39,38 @@ nmap({
     end,
     opts('hatch the [d]uck'),
   },
-  { '<leader>dk', require('duck').cook,                               opts('coo[k] the [d]uck') },
+  { '<Leader>dk', require('duck').cook, opts('coo[k] the [d]uck') },
   -- toggleterm
-  { '<Leader>ot', cmd('ToggleTerm dir=git_dir direction=horizontal'), opts(silent, 'open horizental terminal') },
+  { '<Leader>ot', cmd('ToggleTerm dir=git_dir direction=horizontal'), opts(silent, 'open/close horizental terminal') },
   {
     '<Leader>oo',
     cmd('exe v:count1 . "ToggleTerm dir=git_dir direction=tab"'),
-    opts(silent, 'open terminal in a new tab'),
+    opts(silent, 'open/close terminal in a new tab'),
   },
-  { '<Leader>odo', cmd('LazyDockerToogle'), opts(silent, 'open lazydocker') },
-  { '<Leader>oi',  cmd('IPythonToggle'),    opts(silent, 'open ipython') },
-  { '<Leader>odj', cmd('DjangoShellPlus'),  opts(silent, 'open django shell_plus') },
+  {
+    '<M-0>',
+    cmd('ToggleTermToggleAll'),
+    opts(silent, 'toggle all terminals at once'),
+  },
+  { '<Leader>odo', cmd('LazyDockerToogle'), opts(silent, 'open/close lazydocker') },
+  { '<Leader>oi', cmd('IPythonToggle'), opts(silent, 'open/close ipython') },
+  { '<Leader>odj', cmd('DjangoShellPlus'), opts(silent, 'open/close django shell_plus') },
 })
 
+-- terminal mode key mapping
 tmap({
   '<ESC>',
   '<C-\\><C-n>',
   { silent = true },
 })
+
+for i = 1, 9, 1 do
+  local keymap = {
+    string.format('<M-%d>', i),
+    cmd(string.format('%dToggleTerm dir=git_dir direction=tab', i)),
+    opts(silent, string.format('open/close terminal %d in a new tab', i)),
+  }
+
+  tmap(keymap)
+  nmap(keymap)
+end
