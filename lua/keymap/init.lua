f71420a9a8bea1613378e504cdeeb056e4ba1d03
Parham Alvani <parham.alvani@gmail.com>
2023-07-31 07:29:09 +0330

    feat: handler toogle terminal instance number

--- lua/keymap/init.lua
+++ lua/keymap/init.lua
@@ -13,24 +13,24 @@ local cmd = key.cmd
 -- usage of plugins
 nmap({
   -- packer
-  { '<Leader>pu', cmd('PackerUpdate'), opts(noremap, silent) },
-  { '<Leader>pi', cmd('PackerInstall'), opts(noremap, silent) },
-  { '<Leader>pc', cmd('PackerCompile'), opts(noremap, silent) },
+  { '<Leader>pu',  cmd('PackerUpdate'),           opts(noremap, silent) },
+  { '<Leader>pi',  cmd('PackerInstall'),          opts(noremap, silent) },
+  { '<Leader>pc',  cmd('PackerCompile'),          opts(noremap, silent) },
   -- dashboard
-  { '<Leader>n', cmd('DashboardNewFile'), opts(noremap, silent) },
-  { '<Leader>ss', cmd('SessionSave'), opts(noremap, silent) },
-  { '<Leader>sl', cmd('SessionLoad'), opts(noremap, silent) },
+  { '<Leader>n',   cmd('DashboardNewFile'),       opts(noremap, silent) },
+  { '<Leader>ss',  cmd('SessionSave'),            opts(noremap, silent) },
+  { '<Leader>sl',  cmd('SessionLoad'),            opts(noremap, silent) },
   -- nvimtree
-  { '<Leader>pff', cmd('NvimTreeToggle'), opts(noremap, silent) },
+  { '<Leader>pff', cmd('NvimTreeToggle'),         opts(noremap, silent) },
   -- Telescope
-  { '<Leader>bb', cmd('Telescope buffers'), opts(noremap, silent, 'buffer browser') },
-  { '<Leader>man', cmd('Telescope man_pages'), opts(noremap, silent) },
-  { '<Leader>fa', cmd('Telescope live_grep'), opts(noremap, silent) },
-  { '<Leader>ff', cmd('Telescope file_browser'), opts(noremap, silent, 'file browser') },
-  { '<leader>sf', cmd('Telescope find_files'), opts(noremap, silent, '[s]earch [f]iles') },
+  { '<Leader>bb',  cmd('Telescope buffers'),      opts(noremap, silent, 'buffer browser') },
+  { '<Leader>man', cmd('Telescope man_pages'),    opts(noremap, silent) },
+  { '<Leader>fa',  cmd('Telescope live_grep'),    opts(noremap, silent) },
+  { '<Leader>ff',  cmd('Telescope file_browser'), opts(noremap, silent, 'file browser') },
+  { '<leader>sf',  cmd('Telescope find_files'),   opts(noremap, silent, '[s]earch [f]iles') },
   -- buffers
-  { '<Leader>bn', cmd('bnext'), opts(noremap, silent, 'next buffer') },
-  { '<Leader>bn', cmd('bprevious'), opts(noremap, silent, 'previous buffer') },
+  { '<Leader>bn',  cmd('bnext'),                  opts(noremap, silent, 'next buffer') },
+  { '<Leader>bn',  cmd('bprevious'),              opts(noremap, silent, 'previous buffer') },
   -- Panda
   {
     '<leader>dd',
@@ -39,13 +39,17 @@ nmap({
     end,
     opts('hatch the [d]uck'),
   },
-  { '<leader>dk', require('duck').cook, opts('coo[k] the [d]uck') },
+  { '<leader>dk', require('duck').cook,                               opts('coo[k] the [d]uck') },
   -- toggleterm
   { '<Leader>ot', cmd('ToggleTerm dir=git_dir direction=horizontal'), opts(silent, 'open horizental terminal') },
-  { '<Leader>oo', cmd('ToggleTerm dir=git_dir direction=tab'), opts(silent, 'open terminal in a new tab') },
+  {
+    '<Leader>oo',
+    cmd('exe v:count1 . "ToggleTerm dir=git_dir direction=tab"'),
+    opts(silent, 'open terminal in a new tab'),
+  },
   { '<Leader>odo', cmd('LazyDockerToogle'), opts(silent, 'open lazydocker') },
-  { '<Leader>oi', cmd('IPythonToggle'), opts(silent, 'open ipython') },
-  { '<Leader>odj', cmd('DjangoShellPlus'), opts(silent, 'open django shell_plus') },
+  { '<Leader>oi',  cmd('IPythonToggle'),    opts(silent, 'open ipython') },
+  { '<Leader>odj', cmd('DjangoShellPlus'),  opts(silent, 'open django shell_plus') },
 })
 
 tmap({
