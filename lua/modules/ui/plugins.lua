d330c4aca36afacbe0a90589808442d67d58aa33
Parham Alvani <parham.alvani@gmail.com>
2023-10-08 07:01:41 +0000

    fix: correct lualine configuration loading

--- lua/modules/ui/plugins.lua
+++ lua/modules/ui/plugins.lua
@@ -27,7 +27,7 @@ plugin({
 plugin({
   'nvim-lualine/lualine.nvim',
   requires = { 'nvim-tree/nvim-web-devicons', opt = true },
-  config = conf.lualine(),
+  config = conf.lualine,
 })
 -- A neovim lua plugin to help easily manage multiple terminal windows.
 plugin({
