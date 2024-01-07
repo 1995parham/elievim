9c7ae837cb53d1a22763ce17be78e68eda702dd0
Parham Alvani <parham.alvani@gmail.com>
2024-01-07 05:49:24 +0000

    chore: add just treesitter but it is not working

--- lua/modules/lang/config.lua
+++ lua/modules/lang/config.lua
@@ -22,6 +22,16 @@ function config.nvim_treesitter()
       },
     },
   })
+
+  require('nvim-treesitter.parsers').get_parser_configs().just = {
+    install_info = {
+      url = 'https://github.com/IndianBoy42/tree-sitter-just', -- local path or git repo
+      files = { 'src/parser.c', 'src/scanner.c' },
+      branch = 'main',
+      -- use_makefile = true -- this may be necessary on MacOS (try if you see compiler errors)
+    },
+    maintainers = { '@IndianBoy42' },
+  }
 end
 
 function config.go_nvim()
