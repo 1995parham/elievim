a68c3dcb51de9d1d36aefa89f70128602a476a47
Parham Alvani <parham.alvani@gmail.com>
2024-03-27 08:26:46 +0000

    fix: correct lint issues

--- lua/keymap/plugins.lua
+++ lua/keymap/plugins.lua
@@ -1,9 +1,9 @@
 return {
   {
-  'folke/which-key.nvim',
-  config = function()
-    require('which-key').setup()
-  end,
+    'folke/which-key.nvim',
+    config = function()
+      require('which-key').setup()
+    end,
   },
   { 'tamton-aquib/duck.nvim', lazy = false },
 }
