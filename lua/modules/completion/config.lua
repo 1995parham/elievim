dc7fdf804a8b884052e0d897735e4d75317fa31e
Parham Alvani <parham.alvani@gmail.com>
2024-11-08 05:43:59 +0000

    chore: fix issue with navigator

--- lua/modules/completion/config.lua
+++ lua/modules/completion/config.lua
@@ -50,7 +50,7 @@ function config.navigator()
       hover = {
         enable = true,
       },
-      disable_lsp = { 'pyright' },
+      disable_lsp = { 'pyright', 'ruff_lsp' },
     },
     keymaps = {
       -- https://github.com/ray-x/navigator.lua/blob/master/lua/navigator/lspclient/mapping.lua
