ccd986ad66c3c32269dd81b92a270221ae67e2b2
Parham Alvani <parham.alvani@gmail.com>
2024-12-17 12:52:43 +0000

    fix: remove replace hijack for file telescope

--- lua/modules/tools/config.lua
+++ lua/modules/tools/config.lua
@@ -88,7 +88,6 @@ function config.telescope()
       file_browser = {
         theme = 'ivy',
         -- disables netrw and use telescope-file-browser in its place
-        hijack_netrw = true,
         hidden = true,
         respect_gitignore = true,
         quiet = true,
