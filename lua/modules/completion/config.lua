2bf06086de1a1331cc614d6eb4ddf9e308cbd468
Parham Alvani <parham.alvani@gmail.com>
2025-01-02 10:23:07 +0000

    fix: correct navigator miss configuration

--- lua/modules/completion/config.lua
+++ lua/modules/completion/config.lua
@@ -53,7 +53,7 @@ function config.navigator()
       hover = {
         enable = true,
       },
-      disable_lsp = '__all__',
+      disable_lsp = 'all',
     },
     keymaps = {
       -- https://github.com/ray-x/navigator.lua/blob/master/lua/navigator/lspclient/mapping.lua
