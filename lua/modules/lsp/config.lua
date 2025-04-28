6731807ad219ab2c09096458a9cab0764152f83a
Parham Alvani <parham.alvani@gmail.com>
2025-04-28 00:01:03 +0000

    fix: correct lint issues

--- lua/modules/lsp/config.lua
+++ lua/modules/lsp/config.lua
@@ -262,54 +262,53 @@ function config.cmp()
         draw = {
           -- We don't need label_description now because label and label_description are already
           -- combined together in label by colorful-menu.nvim.
-          columns = { { "kind_icon" }, { "label", gap = 1 } },
+          columns = { { 'kind_icon' }, { 'label', gap = 1 } },
           components = {
-              label = {
-                  text = function(ctx)
-                      return require("colorful-menu").blink_components_text(ctx)
-                  end,
-                  highlight = function(ctx)
-                      return require("colorful-menu").blink_components_highlight(ctx)
-                  end,
-              },
-              kind_icon = {
-                text = function(ctx)
-                  local lspkind = require("lspkind")
-                  local icon = ctx.kind_icon
-                  if vim.tbl_contains({ "Path" }, ctx.source_name) then
-                      local dev_icon, _ = require("nvim-web-devicons").get_icon(ctx.label)
-                      if dev_icon then
-                          icon = dev_icon
-                      end
-                  else
-                      icon = require("lspkind").symbolic(ctx.kind, {
-                          mode = "symbol",
-                      })
+            label = {
+              text = function(ctx)
+                return require('colorful-menu').blink_components_text(ctx)
+              end,
+              highlight = function(ctx)
+                return require('colorful-menu').blink_components_highlight(ctx)
+              end,
+            },
+            kind_icon = {
+              text = function(ctx)
+                local icon = ctx.kind_icon
+                if vim.tbl_contains({ 'Path' }, ctx.source_name) then
+                  local dev_icon, _ = require('nvim-web-devicons').get_icon(ctx.label)
+                  if dev_icon then
+                    icon = dev_icon
                   end
+                else
+                  icon = require('lspkind').symbolic(ctx.kind, {
+                    mode = 'symbol',
+                  })
+                end
 
-                  return icon .. ctx.icon_gap
-                end,
-                -- Optionally, use the highlight groups from nvim-web-devicons
-                -- You can also add the same function for `kind.highlight` if you want to
-                -- keep the highlight groups in sync with the icons.
-                highlight = function(ctx)
-                  local hl = ctx.kind_hl
-                  if vim.tbl_contains({ "Path" }, ctx.source_name) then
-                    local dev_icon, dev_hl = require("nvim-web-devicons").get_icon(ctx.label)
-                    if dev_icon then
-                      hl = dev_hl
-                    end
+                return icon .. ctx.icon_gap
+              end,
+              -- Optionally, use the highlight groups from nvim-web-devicons
+              -- You can also add the same function for `kind.highlight` if you want to
+              -- keep the highlight groups in sync with the icons.
+              highlight = function(ctx)
+                local hl = ctx.kind_hl
+                if vim.tbl_contains({ 'Path' }, ctx.source_name) then
+                  local dev_icon, dev_hl = require('nvim-web-devicons').get_icon(ctx.label)
+                  if dev_icon then
+                    hl = dev_hl
                   end
-                  return hl
-                end,
-              },
+                end
+                return hl
+              end,
             },
           },
         },
+      },
       documentation = {
         auto_show = true,
         auto_show_delay_ms = 500,
-      }
+      },
     },
 
     -- Default list of enabled providers defined so that you can extend it
